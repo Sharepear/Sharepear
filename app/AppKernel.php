0ec3175ba429992e4a16698e5f3c504391fa674d
Simon Constans <kosssi@gmail.com>
2014-01-04 17:23:06 +0100

    Installs OneupUploaderBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new kosssi\MyAlbumsBundle\kosssiMyAlbumsBundle(),
             new Vlabs\MediaBundle\VlabsMediaBundle(),
+            new Oneup\UploaderBundle\OneupUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
