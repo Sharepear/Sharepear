b6d3520b37ebbc51151d51d1c94bd1f0844850af
Simon Constans <kosssi@gmail.com>
2014-02-23 19:54:20 +0100

    Add test for filter config

---
+++ src/kosssi/MyAlbumsBundle/Tests/LiipImagine/FilterConfigTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace kosssi\MyAlbumsBundle\Tests\LiipImagine;
+
+/**
+ * Class FilterConfigTest
+ *
+ * @author Simon Constans <kosssi@gmail.com>
+ */
+class FilterConfigTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * test function getFiltersNames with no configuration
+     */
+    public function testGetFiltersNamesWithEmptyConfig()
+    {
+        $filterConfig = new FilterConfig(array());
+        $filtersNames = $filterConfig->getFiltersNames();
+
+        $this->assertEquals(array(), $filtersNames);
+    }
+
+    /**
+     * test function getFiltersNames with configuration
+     */
+    public function testGetFiltersNamesWithConfig()
+    {
+        $filterConfig = new FilterConfig(array('filterName1' => null, 'filterName2' => null));
+        $filtersNames = $filterConfig->getFiltersNames();
+
+        $this->assertEquals(array('filterName1', 'filterName2'), $filtersNames);
+    }
+}
