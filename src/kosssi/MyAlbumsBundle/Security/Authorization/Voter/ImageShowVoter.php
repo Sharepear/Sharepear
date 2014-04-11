dc9b9294fcca4603a6c718c7af9d691818e91abd
Simon Constans <simonc@theodo.fr>
2014-04-11 19:33:47 +0200

    Add voter for Image

---
+++ src/kosssi/MyAlbumsBundle/Security/Authorization/Voter/ImageShowVoter.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace kosssi\MyAlbumsBundle\Security\Authorization\Voter;
+
+use kosssi\MyAlbumsBundle\Entity\Image;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class ImageShowVoter implements VoterInterface
+{
+    /**
+     * @param string $attribute
+     *
+     * @return bool
+     */
+    public function supportsAttribute($attribute)
+    {
+        return 'IMAGE_SHOW' === $attribute;
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return $class instanceof Image;
+    }
+
+    /**
+     * @param TokenInterface $token
+     * @param Image          $object
+     * @param array          $attributes
+     *
+     * @return int
+     */
+    function vote(TokenInterface $token, $object, array $attributes)
+    {
+        foreach ($attributes as $attribute) {
+            if ($this->supportsAttribute($attribute) && $this->supportsClass($object)) {
+                $user = $token->getUser();
+
+                if ($object instanceof Image && $user == $object->getUser()) {
+                    return VoterInterface::ACCESS_GRANTED;
+                }
+            }
+        }
+
+        return VoterInterface::ACCESS_DENIED;
+    }
+}
