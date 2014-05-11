874b818408bbee2a167bc1b6d5da9f6fdef0de44
Simon Constans <kosssi@gmail.com>
2014-05-11 18:26:34 +0200

    Complete ImageOptimiseHelper test

--- src/kosssi/MyAlbumsBundle/Tests/Helper/ImageOptimiseHelperTest.php
+++ src/kosssi/MyAlbumsBundle/Tests/Helper/ImageOptimiseHelperTest.php
@@ -2,7 +2,12 @@
 
 namespace kosssi\MyAlbumsBundle\Tests\Helper;
 
+use Imagine\Gmagick\Imagine;
+use Imagine\Image\ImageInterface;
 use kosssi\MyAlbumsBundle\Helper\ImageOptimiseHelper;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
+use Liip\ImagineBundle\Imagine\Filter\FilterConfiguration;
+use Phake;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -37,5 +42,30 @@ class ImageOptimiseHelperTest extends WebTestCase
         $this->assertLessThan(filesize($imagePath), filesize($imageOptimisePath));
 
         $fs->remove($imageOptimisePath);
+    }
+
+    /**
+     * test optimiseCaches
+     */
+    public function testOptimiseCaches()
+    {
+        $rootDir = "";
+        $imagine = Phake::mock(Imagine::class);
+        $cacheManager = Phake::mock(CacheManager::class);
+        $filterConfig = Phake::mock(FilterConfiguration::class);
+        $path = "/test.jpg";
+        $filterName = "xl";
+        $filters = [$filterName => null];
+        $imageInterface = Phake::mock(ImageInterface::class);
+
+        Phake::when($filterConfig)->all()->thenReturn($filters);
+        Phake::when($cacheManager)->resolve($path, $filterName)->thenReturn("/blabla/media/test/me.jpg");
+        Phake::when($imagine)->open(Phake::anyParameters())->thenReturn($imageInterface);
+
+        $imageOptimiseHelper = new ImageOptimiseHelper($rootDir, $imagine, $cacheManager, $filterConfig);
+        $imageOptimiseHelper->optimiseCaches($path);
+
+        Phake::verify($cacheManager, Phake::times(1))->resolve($path, $filterName);
+        Phake::verify($imagine, Phake::times(1))->open(Phake::anyParameters());
     }
 }
