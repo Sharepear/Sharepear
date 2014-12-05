8df417274fab60d3a247103f809e4493e3f17d6e
Simon Constans <simonc@linux.com>
2014-12-05 01:05:13 +0100

    Add share album

--- src/kosssi/MyAlbumsBundle/Controller/AlbumController.php
+++ src/kosssi/MyAlbumsBundle/Controller/AlbumController.php
@@ -135,7 +135,7 @@ public function downloadAction(Album $album)
     * @Config\Template()
     * @Config\Security("is_granted('ALBUM_EDIT', album)")
     *
-    * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+    * @return \Symfony\Component\HttpFoundation\RedirectResponse
     */
     public function deleteAction(Album $album)
     {
@@ -144,5 +144,30 @@ public function downloadAction(Album $album)
         $em->flush();
 
         return $this->redirect($this->generateUrl('homepage'));
+    }
+
+    /**
+     * @param Album   $album
+     * @param Boolean $share
+     *
+     * @Config\Route("/{id}/share", name="album_share", defaults={"share": true})
+     * @Config\Route("/{id}/unshare", name="album_unshare", defaults={"share": false})
+     * @Config\Template()
+     * @Config\Security("is_granted('ALBUM_EDIT', album)")
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function shareAction(Album $album, $share)
+    {
+        $album->setPublic($share);
+        foreach ($album->getImages() as $image) {
+            $image->setPublic($share);
+        }
+
+        $em = $this->get('doctrine.orm.default_entity_manager');
+        $em->persist($album);
+        $em->flush();
+
+        return $this->redirect($this->generateUrl('album_show', array('id' => $album->getId())));
     }
 }
